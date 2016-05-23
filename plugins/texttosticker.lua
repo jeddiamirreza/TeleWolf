2708ad0ee27923a6f11d13ca83177ff24c8273b2
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-23 18:38:03 +0430

    Update texttosticker.lua

--- plugins/texttosticker.lua
+++ plugins/texttosticker.lua
@@ -1,37 +1,50 @@
 do
 
 local function run(msg, matches)
-
-
-local text = matches[1]
-
-  local b = 1
-
-  while b ~= 0 do
-    textc = text:trim()
-    text,b = text:gsub(' ','.')
-    
-    
-  if msg.to.type == 'user' then 
-      return
-      else
-  local url = "http://latex.codecogs.com/png.download?".."\\dpi{800}%20\\LARGE%20"..textc
-  local receiver = get_receiver(msg)
-  local file = download_to_file(url,'text.webp')
-      send_audio('channel#id'..msg.to.id, file, ok_cb , false)
+  local eq = URL.escape(matches[2])
+local w = "640"
+local h = "200"
+local txtsize = "62"
+local txtclr = "ff2e4357"
+if matches[3] then 
+  txtclr = matches[3]
 end
-end
+if matches[4] then 
+  txtsize = matches[4]
   end
+  if matches[5] and matches[6] then 
+  w = matches[5]
+  h = matches[6]
+  end
+  local url = "https://assets.imgix.net/examples/clouds.jpg?blur=150&w="..w.."&h="..h.."&fit=crop&txt="..eq.."&txtsize="..txtsize.."&txtclr="..txtclr.."&txtalign=middle,center&txtfont=Futura%20Condensed%20Medium&mono=ff6598cc"
+
+  local receiver = get_receiver(msg)
+  if matches[1] == "*" then 
+  send_photo_from_url(receiver, url, send_title, {receiver, title})
+else
+local  file = download_to_file(url,'text.webp')
+ send_document('channel#id'..msg.to.id, file, ok_cb , false)
+end
+end
+
 return {
-  description = "text to sticker",
+  description = "Convert Text to Image",
   usage = {
-    "!sticker [text]"
+    "/conv (txt) : convert txt to img"
   },
   patterns = {
-    "^[!/#]sticker +(.*)$",
-
+    "^[!/#$]sticker(*) (.+)$",
+    "^[!/#$](sticker) (.+)$",
+    "^[!/#$]sticker(*) (.+) (.+)$",
+    "^[!/#$]sticker (3) (.+) (.+) (.+)$",
+        "^[!/#$]sticker(*)2 (.+) (.+)$",
+        "^[!/#$]sticker (2) (.+) (.+)$",
+    "^[!/#$]sticker(*)3 (.+) (.+) (.+)$", 
+    "^[!/#$]sticker(*)4 (.+) (.+) (.+) (.+) (.+)$",
+    "^[!/#$]sticker (4) (.+) (.+) (.+) (.+) (.+)$"
   },
   run = run
 }
 
 end
+
