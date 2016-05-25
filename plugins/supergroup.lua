990935d1ab23fe6cc6b9081ea0c693e6544409b8
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-25 13:03:40 +0430

    Update supergroup.lua

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -1268,7 +1268,7 @@ local function run(msg, matches)
 				resolve_username(username,  callbackres, cbres_extra)
 			else
 				savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested SuperGroup ID")
-				return "🔰 SuperGroup Name: " ..string.gsub(msg.to.print_name, "_", " ").. "\n🔰 SuperGroup Id: "..msg.to.id.."\n⚠️ Your Name: "..msg.from.print_name.."\n⚠️ Your Id: "..msg.from.id.."\n⚠️ Your UserName: "..msg.from.username.."\n⚠️ Your TelegramLink: telegram.me/"..msg.from.username
+				return "🔰 SuperGroup Name: " ..string.gsub(msg.to.print_name, "_", " ").. "\n🔰 SuperGroup Id: "..msg.to.id.."\n⚠️ Your Name: "..msg.from.print_name.."\n⚠️ Your Id: "..msg.from.id.."\n⚠️ Your UserName: @"..msg.from.username.."\n⚠️ Your TelegramLink: telegram.me/"..msg.from.username
 			end
 		end
 
