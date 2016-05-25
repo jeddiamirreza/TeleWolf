7a2035d7f0028234e587c6e1b05a5f102ba2a1fa
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-25 11:23:35 +0430

    Create info.lua

---
+++ plugins/info.lua
@@ -0,0 +1,22 @@
+local function run(msg, matches)
+
+if matches[1]:lower() == 'info' then
+return '🔰 Your Name: '..msg.from.print_name..'\n🔰 Your Id: '..msg.from.id..'\n🔰 Yourt User Naem: @'..msg.from.username..'\n🔰 Your Phone Number: '..msg.from.phone..'\n🔰 Your TelegramLink: telegram.me/'..msg.from.username..'\n⚠️ Group Name: '..msg.to.print_name..'\n⚠️ Group Id: '..msg.to.id..'⚠️ Text Messages: '..msg.text
+end
+end
+
+return {
+
+patterns = {
+
+
+"^[!#$/]([Ii]nfo)$",
+
+
+},
+
+
+run = run
+
+
+}
