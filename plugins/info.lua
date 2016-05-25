d6b072e372fc06b7e23bc8b5a459c14cdea78d3d
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-25 12:51:44 +0430

    Update info.lua

--- plugins/info.lua
+++ plugins/info.lua
@@ -1,7 +1,7 @@
 local function run(msg, matches)
 
 if matches[1]:lower() == 'info' then
-return '🔰 Your Name: '..msg.from.print_name..'\n🔰 Your Id: '..msg.from.id..'\n🔰 Yourt User Naem: @'..msg.from.username..'\n🔰 Your Phone Number: '..msg.from.phone..'\n🔰 Your TelegramLink: telegram.me/'..msg.from.username..'\n⚠️ Group Name: '..msg.to.print_name..'\n⚠️ Group Id: '..msg.to.id..'⚠️ Text Messages: '..msg.text
+return '🔰 Your Name: '..msg.from.print_name..'\n🔰 Your Id: '..msg.from.id..'\n🔰 Yourt User Naem: @'..msg.from.username..'\n🔰 Your Phone Number: +'..msg.from.phone..'\n🔰 Your TelegramLink: telegram.me/'..msg.from.username..'\n⚠️ Group Name: '..msg.to.print_name..'\n⚠️ Group Id: '..msg.to.id..'\n⚠️ Text Messages: '..msg.text
 end
 end
 
@@ -20,3 +20,4 @@ run = run
 
 
 }
+--By @MR_Mh58
