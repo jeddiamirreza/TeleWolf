ec1a59c1229e7b5e5328b9147a103ae9940577b1
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-27 20:08:27 +0430

    Update LinkPv.lua

--- plugins/LinkPv.lua
+++ plugins/LinkPv.lua
@@ -2,14 +2,14 @@ do
 
 function run(msg, matches)
        if not is_owner(msg) then
-        return "⚠️ You do not have access"
+        return "⚠️ You Do Not Have Access"
        end
     local data = load_data(_config.moderation.data)
       local group_link = data[tostring(msg.to.id)]['settings']['set_link']
        if not group_link then 
         return "⚠️ Create New Link With /newlink Command"
        end
-         local text = "SuperGroup link  :\n"..group_link
+         local text = "⚠️ Group Link For "..msg.to.print_name.."\n"..group_link
           send_large_msg('user#id'..msg.from.id, text.."\n", ok_cb, false)
            return "🔰 Group Link Sent To Pv"
 end
@@ -22,3 +22,4 @@ return {
 }
 
 end
+--By @Mr_Mh58
