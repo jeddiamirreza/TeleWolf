44388a5560c4bf7ad8ef194f4a76a01629af00f4
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-29 14:16:09 +0430

    Create helpowner.lua

---
+++ plugins/helpowner.lua
@@ -0,0 +1,82 @@
+local function run(msg, matches)
+
+
+if is_owner(msg) matches[1]:lower() == 'help' then
+if matches[2]:lower() == 'tools' then
+return [[
+
+🔰 Trojan Bot Tools (Owner Tools Help) :
+
+
+⚠️ Coin In Now :
+#coin
+
+⚠️ Mean Word :
+#mean [Word]
+
+⚠️ Link Shorter :
+#short [URl]
+
+⚠️ Sticker Maker :
+#sticker [Text]
+
+⚠️ Text To Voice : 
+#voice [Text]
+
+⚠️ Weather Condition :
+#weather [location]
+
+⚠️ Music List :
+#music [Artist Name Or Song Name]
+
+⚠️ Music Downloader :
+#dl [Music Number]
+
+⚠️ Get Jokes
+#joke
+
+⚠️ Calculator
+#calc [Formula]
+
+⚠️ Downloade Command :
+#download [URl]
+
+🔰 The Following File Formats Are Supported
+Gif , Mp3 , Pdf , Ogg , Mp4 , Zip , Rar , Wmv , Avi , Webp
+
+⚠️ Get Support Group Link :
+#splink
+
+⚠️ Text To Painting :
+#painting [Text]
+
+⚠️ UnShort Link :
+#unshort [URl]
+
+⚠️ Get the text under the photo :
+#cap [Ripley's photo]
+
+⚠️ Writing Text Beautiful :
+#text [Text]
+
+-------------------------------------
+☢ I Am Trojan Bot
+-------------------------------------
+]]
+end
+end
+
+
+return {
+
+patterns = {
+
+"^[!#$/]([Hh]elp) (.*)$",
+
+
+
+},
+
+run = run
+
+}
