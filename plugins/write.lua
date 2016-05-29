56e899a41ce5203b4e0c2e6cfa85bd5a5c5a2ce2
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-29 14:03:12 +0430

    Create write.lua

---
+++ plugins/write.lua
@@ -0,0 +1,120 @@
+local function run(msg, matches)
+	
+	if #matches < 2 then
+		return "بعد از این دستور، با قید یک فاصله کلمه یا جمله ی مورد نظر را جهت زیبا نویسی وارد کنید"
+	end
+	if string.len(matches[2]) > 20 then
+		return "حداکثر حروف مجاز 20 کاراکتر انگلیسی و عدد است"
+	end
+	local font_base = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,0,9,8,7,6,5,4,3,2,1,.,_"
+	local font_hash = "z,y,x,w,v,u,t,s,r,q,p,o,n,m,l,k,j,i,h,g,f,e,d,c,b,a,Z,Y,X,W,V,U,T,S,R,Q,P,O,N,M,L,K,J,I,H,G,F,E,D,C,B,A,0,1,2,3,4,5,6,7,8,9,.,_"
+	local fonts = {
+		"ⓐ,ⓑ,ⓒ,ⓓ,ⓔ,ⓕ,ⓖ,ⓗ,ⓘ,ⓙ,ⓚ,ⓛ,ⓜ,ⓝ,ⓞ,ⓟ,ⓠ,ⓡ,ⓢ,ⓣ,ⓤ,ⓥ,ⓦ,ⓧ,ⓨ,ⓩ,ⓐ,ⓑ,ⓒ,ⓓ,ⓔ,ⓕ,ⓖ,ⓗ,ⓘ,ⓙ,ⓚ,ⓛ,ⓜ,ⓝ,ⓞ,ⓟ,ⓠ,ⓡ,ⓢ,ⓣ,ⓤ,ⓥ,ⓦ,ⓧ,ⓨ,ⓩ,⓪,➈,➇,➆,➅,➄,➃,➂,➁,➀,●,_",
+		"⒜,⒝,⒞,⒟,⒠,⒡,⒢,⒣,⒤,⒥,⒦,⒧,⒨,⒩,⒪,⒫,⒬,⒭,⒮,⒯,⒰,⒱,⒲,⒳,⒴,⒵,⒜,⒝,⒞,⒟,⒠,⒡,⒢,⒣,⒤,⒥,⒦,⒧,⒨,⒩,⒪,⒫,⒬,⒭,⒮,⒯,⒰,⒱,⒲,⒳,⒴,⒵,⓪,⑼,⑻,⑺,⑹,⑸,⑷,⑶,⑵,⑴,.,_",
+		"α,в,c,∂,є,ƒ,g,н,ι,נ,к,ℓ,м,η,σ,ρ,q,я,ѕ,т,υ,ν,ω,χ,у,z,α,в,c,∂,є,ƒ,g,н,ι,נ,к,ℓ,м,η,σ,ρ,q,я,ѕ,т,υ,ν,ω,χ,у,z,0,9,8,7,6,5,4,3,2,1,.,_",
+		"α,в,c,d,e,ғ,ɢ,н,ι,j,ĸ,l,м,ɴ,o,p,q,r,ѕ,т,υ,v,w,х,y,z,α,в,c,d,e,ғ,ɢ,н,ι,j,ĸ,l,м,ɴ,o,p,q,r,ѕ,т,υ,v,w,х,y,z,0,9,8,7,6,5,4,3,2,1,.,_",
+		"α,в,¢,đ,e,f,g,ħ,ı,נ,κ,ł,м,и,ø,ρ,q,я,š,т,υ,ν,ω,χ,ч,z,α,в,¢,đ,e,f,g,ħ,ı,נ,κ,ł,м,и,ø,ρ,q,я,š,т,υ,ν,ω,χ,ч,z,0,9,8,7,6,5,4,3,2,1,.,_",
+		"ą,ҍ,ç,ժ,ҽ,ƒ,ց,հ,ì,ʝ,ҟ,Ӏ,ʍ,ղ,օ,ք,զ,ɾ,ʂ,է,մ,ѵ,ա,×,վ,Հ,ą,ҍ,ç,ժ,ҽ,ƒ,ց,հ,ì,ʝ,ҟ,Ӏ,ʍ,ղ,օ,ք,զ,ɾ,ʂ,է,մ,ѵ,ա,×,վ,Հ,⊘,९,𝟠,7,Ϭ,Ƽ,५,Ӡ,ϩ,𝟙,.,_",		"ค,ც,८,ძ,૯,Բ,૭,Һ,ɿ,ʆ,қ,Ն,ɱ,Ո,૦,ƿ,ҩ,Ր,ς,੮,υ,౮,ω,૪,ע,ઽ,ค,ც,८,ძ,૯,Բ,૭,Һ,ɿ,ʆ,қ,Ն,ɱ,Ո,૦,ƿ,ҩ,Ր,ς,੮,υ,౮,ω,૪,ע,ઽ,0,9,8,7,6,5,4,3,2,1,.,_",
+		"α,ß,ς,d,ε,ƒ,g,h,ï,յ,κ,ﾚ,m,η,⊕,p,Ω,r,š,†,u,∀,ω,x,ψ,z,α,ß,ς,d,ε,ƒ,g,h,ï,յ,κ,ﾚ,m,η,⊕,p,Ω,r,š,†,u,∀,ω,x,ψ,z,0,9,8,7,6,5,4,3,2,1,.,_",
+		"ค,๒,ς,๔,є,Ŧ,ɠ,ђ,เ,ן,к,l,๓,ภ,๏,թ,ợ,г,ร,t,ย,v,ฬ,x,ץ,z,ค,๒,ς,๔,є,Ŧ,ɠ,ђ,เ,ן,к,l,๓,ภ,๏,թ,ợ,г,ร,t,ย,v,ฬ,x,ץ,z,0,9,8,7,6,5,4,3,2,1,.,_",
+		"ﾑ,乃,ζ,Ð,乇,ｷ,Ǥ,ん,ﾉ,ﾌ,ズ,ﾚ,ᄊ,刀,Ծ,ｱ,Q,尺,ㄎ,ｲ,Ц,Џ,Щ,ﾒ,ﾘ,乙,ﾑ,乃,ζ,Ð,乇,ｷ,Ǥ,ん,ﾉ,ﾌ,ズ,ﾚ,ᄊ,刀,Ծ,ｱ,q,尺,ㄎ,ｲ,Ц,Џ,Щ,ﾒ,ﾘ,乙,ᅙ,9,8,ᆨ,6,5,4,3,ᆯ,1,.,_",
+		"α,β,c,δ,ε,Ŧ,ĝ,h,ι,j,κ,l,ʍ,π,ø,ρ,φ,Ʀ,$,†,u,υ,ω,χ,ψ,z,α,β,c,δ,ε,Ŧ,ĝ,h,ι,j,κ,l,ʍ,π,ø,ρ,φ,Ʀ,$,†,u,υ,ω,χ,ψ,z,0,9,8,7,6,5,4,3,2,1,.,_",
+		"ձ,ъ,ƈ,ժ,ε,բ,ց,հ,ﻨ,յ,ĸ,l,ო,ռ,օ,թ,զ,г,ร,է,ս,ν,ա,×,ყ,২,ձ,ъ,ƈ,ժ,ε,բ,ց,հ,ﻨ,յ,ĸ,l,ო,ռ,օ,թ,զ,г,ร,է,ս,ν,ա,×,ყ,২,0,9,8,7,6,5,4,3,2,1,.,_",
+		"Λ,ɓ,¢,Ɗ,£,ƒ,ɢ,ɦ,ĩ,ʝ,Қ,Ł,ɱ,ה,ø,Ṗ,Ҩ,Ŕ,Ş,Ŧ,Ū,Ɣ,ω,Ж,¥,Ẑ,Λ,ɓ,¢,Ɗ,£,ƒ,ɢ,ɦ,ĩ,ʝ,Қ,Ł,ɱ,ה,ø,Ṗ,Ҩ,Ŕ,Ş,Ŧ,Ū,Ɣ,ω,Ж,¥,Ẑ,0,9,8,7,6,5,4,3,2,1,.,_",
+		"Λ,Б,Ͼ,Ð,Ξ,Ŧ,G,H,ł,J,К,Ł,M,Л,Ф,P,Ǫ,Я,S,T,U,V,Ш,Ж,Џ,Z,Λ,Б,Ͼ,Ð,Ξ,Ŧ,g,h,ł,j,К,Ł,m,Л,Ф,p,Ǫ,Я,s,t,u,v,Ш,Ж,Џ,z,0,9,8,7,6,5,4,3,2,1,.,_",
+		"ɐ,q,ɔ,p,ǝ,ɟ,ɓ,ɥ,ı,ſ,ʞ,ๅ,ɯ,u,o,d,b,ɹ,s,ʇ,n,ʌ,ʍ,x,ʎ,z,ɐ,q,ɔ,p,ǝ,ɟ,ɓ,ɥ,ı,ſ,ʞ,ๅ,ɯ,u,o,d,b,ɹ,s,ʇ,n,ʌ,ʍ,x,ʎ,z,0,9,8,7,6,5,4,3,2,1,.,_",
+		"ɒ,d,ɔ,b,ɘ,ʇ,ϱ,н,i,į,ʞ,l,м,и,o,q,p,я,ƨ,т,υ,v,w,x,γ,z,ɒ,d,ɔ,b,ɘ,ʇ,ϱ,н,i,į,ʞ,l,м,и,o,q,p,я,ƨ,т,υ,v,w,x,γ,z,0,9,8,7,6,5,4,3,2,1,.,_",
+		"A̴,̴B̴,̴C̴,̴D̴,̴E̴,̴F̴,̴G̴,̴H̴,̴I̴,̴J̴,̴K̴,̴L̴,̴M̴,̴N̴,̴O̴,̴P̴,̴Q̴,̴R̴,̴S̴,̴T̴,̴U̴,̴V̴,̴W̴,̴X̴,̴Y̴,̴Z̴,̴a̴,̴b̴,̴c̴,̴d̴,̴e̴,̴f̴,̴g̴,̴h̴,̴i̴,̴j̴,̴k̴,̴l̴,̴m̴,̴n̴,̴o̴,̴p̴,̴q̴,̴r̴,̴s̴,̴t̴,̴u̴,̴v̴,̴w̴,̴x̴,̴y̴,̴z̴,̴0̴,̴9̴,̴8̴,̴7̴,̴6̴,̴5̴,̴4̴,̴3̴,̴2̴,̴1̴,̴.̴,̴_̴",
+		"【【A】】,【【B】】,【【C】】,【【D】】,【【E】】,【【F】】,【【G】】,【【H】】,【【I】】,【【J】】,【【K】】,【【L】】,【【M】】,【【N】】,【【O】】,【【P】】,【【Q】】,【【R】】,【【S】】,【【T】】,【【U】】,【【V】】,【【W】】,【【X】】,【【Y】】,【【Z】】,【【a】】,【【b】】,【【c】】,【【d】】,【【e】】,【【f】】,【【g】】,【【h】】,【【i】】,【【j】】,【【k】】,【【l】】,【【m】】,【【n】】,【【o】】,【【p】】,【【q】】,【【r】】,【【s】】,【【t】】,【【u】】,【【v】】,【【w】】,【【x】】,【【y】】,【【z】】,0,9,8,7,6,5,4,3,2,1,.,_",
+		"『『A』』,『『B』』,『『C』』,『『D』』,『『E』』,『『F』』,『『G』』,『『H』』,『『I』』,『『J』』,『『K』』,『『L』』,『『M』』,『『N』』,『『O』』,『『P』』,『『Q』』,『『R』』,『『S』』,『『T』』,『『U』』,『『V』』,『『W』』,『『X』』,『『Y』』,『『Z』』,『『a』』,『『b』』,『『c』』,『『d』』,『『e』』,『『f』』,『『g』』,『『h』』,『『i』』,『『j』』,『『k』』,『『l』』,『『m』』,『『n』』,『『o』』,『『p』』,『『q』』,『『r』』,『『s』』,『『t』』,『『u』』,『『v』』,『『w』』,『『x』』,『『y』』,『『z』』,0,9,8,7,6,5,4,3,2,1,.,_",
+		"[̲̅[̲̅A̲̅]̲̅],[̲̅[̲̅B̲̅]̲̅],[̲̅[̲̅C̲̅]̲̅],[̲̅[̲̅D̲̅]̲̅],[̲̅[̲̅E̲̅]̲̅],[̲̅[̲̅F̲̅]̲̅],[̲̅[̲̅G̲̅]̲̅],[̲̅[̲̅H̲̅]̲̅],[̲̅[̲̅I̲̅]̲̅],[̲̅[̲̅J̲̅]̲̅],[̲̅[̲̅K̲̅]̲̅],[̲̅[̲̅L̲̅]̲̅],[̲̅[̲̅M̲̅]̲̅],[̲̅[̲̅N̲̅]̲̅],[̲̅[̲̅O̲̅]̲̅],[̲̅[̲̅P̲̅]̲̅],[̲̅[̲̅Q̲̅]̲̅],[̲̅[̲̅R̲̅]̲̅],[̲̅[̲̅S̲̅]̲̅],[̲̅[̲̅T̲̅]̲̅],[̲̅[̲̅U̲̅]̲̅],[̲̅[̲̅V̲̅]̲̅],[̲̅[̲̅W̲̅]̲̅],[̲̅[̲̅X̲̅]̲̅],[̲̅[̲̅Y̲̅]̲̅],[̲̅[̲̅Z̲̅]̲̅],[̲̅[̲̅a̲̅]̲̅],[̲̅[̲̅b̲̅]̲̅],[̲̅[̲̅c̲̅]̲̅],[̲̅[̲̅d̲̅]̲̅],[̲̅[̲̅e̲̅]̲̅],[̲̅[̲̅f̲̅]̲̅],[̲̅[̲̅g̲̅]̲̅],[̲̅[̲̅h̲̅]̲̅],[̲̅[̲̅i̲̅]̲̅],[̲̅[̲̅j̲̅]̲̅],[̲̅[̲̅k̲̅]̲̅],[̲̅[̲̅l̲̅]̲̅],[̲̅[̲̅m̲̅]̲̅],[̲̅[̲̅n̲̅]̲̅],[̲̅[̲̅o̲̅]̲̅],[̲̅[̲̅p̲̅]̲̅],[̲̅[̲̅q̲̅]̲̅],[̲̅[̲̅r̲̅]̲̅],[̲̅[̲̅s̲̅]̲̅],[̲̅[̲̅t̲̅]̲̅],[̲̅[̲̅u̲̅]̲̅],[̲̅[̲̅v̲̅]̲̅],[̲̅[̲̅w̲̅]̲̅],[̲̅[̲̅x̲̅]̲̅],[̲̅[̲̅y̲̅]̲̅],[̲̅[̲̅z̲̅]̲̅],0,9,8,7,6,5,4,3,2,1,.,_",
+	}
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	local result = {}
+	i=0
+	for k=1,#fonts do
+		i=i+1
+		local tar_font = fonts[i]:split(",")
+		local text = matches[2]
+		local text = text:gsub("A",tar_font[1])
+		local text = text:gsub("B",tar_font[2])
+		local text = text:gsub("C",tar_font[3])
+		local text = text:gsub("D",tar_font[4])
+		local text = text:gsub("E",tar_font[5])
+		local text = text:gsub("F",tar_font[6])
+		local text = text:gsub("G",tar_font[7])
+		local text = text:gsub("H",tar_font[8])
+		local text = text:gsub("I",tar_font[9])
+		local text = text:gsub("J",tar_font[10])
+		local text = text:gsub("K",tar_font[11])
+		local text = text:gsub("L",tar_font[12])
+		local text = text:gsub("M",tar_font[13])
+		local text = text:gsub("N",tar_font[14])
+		local text = text:gsub("O",tar_font[15])
+		local text = text:gsub("P",tar_font[16])
+		local text = text:gsub("Q",tar_font[17])
+		local text = text:gsub("R",tar_font[18])
+		local text = text:gsub("S",tar_font[19])
+		local text = text:gsub("T",tar_font[20])
+		local text = text:gsub("U",tar_font[21])
+		local text = text:gsub("V",tar_font[22])
+		local text = text:gsub("W",tar_font[23])
+		local text = text:gsub("X",tar_font[24])
+		local text = text:gsub("Y",tar_font[25])
+		local text = text:gsub("Z",tar_font[26])
+		local text = text:gsub("a",tar_font[27])
+		local text = text:gsub("b",tar_font[28])
+		local text = text:gsub("c",tar_font[29])
+		local text = text:gsub("d",tar_font[30])
+		local text = text:gsub("e",tar_font[31])
+		local text = text:gsub("f",tar_font[32])
+		local text = text:gsub("g",tar_font[33])
+		local text = text:gsub("h",tar_font[34])
+		local text = text:gsub("i",tar_font[35])
+		local text = text:gsub("j",tar_font[36])
+		local text = text:gsub("k",tar_font[37])
+		local text = text:gsub("l",tar_font[38])
+		local text = text:gsub("m",tar_font[39])
+		local text = text:gsub("n",tar_font[40])
+		local text = text:gsub("o",tar_font[41])
+		local text = text:gsub("p",tar_font[42])
+		local text = text:gsub("q",tar_font[43])
+		local text = text:gsub("r",tar_font[44])
+		local text = text:gsub("s",tar_font[45])
+		local text = text:gsub("t",tar_font[46])
+		local text = text:gsub("u",tar_font[47])
+		local text = text:gsub("v",tar_font[48])
+		local text = text:gsub("w",tar_font[49])
+		local text = text:gsub("x",tar_font[50])
+		local text = text:gsub("y",tar_font[51])
+		local text = text:gsub("z",tar_font[52])
+		local text = text:gsub("0",tar_font[53])
+		local text = text:gsub("9",tar_font[54])
+		local text = text:gsub("8",tar_font[55])
+		local text = text:gsub("7",tar_font[56])
+		local text = text:gsub("6",tar_font[57])
+		local text = text:gsub("5",tar_font[58])
+		local text = text:gsub("4",tar_font[59])
+		local text = text:gsub("3",tar_font[60])
+		local text = text:gsub("2",tar_font[61])
+		local text = text:gsub("1",tar_font[62])
+
+		table.insert(result, text)
+	end
+	local result_text = "⚠️ Original Text : "..matches[2].."\n🔰 Designed by "..tostring(#fonts).." Font :\n______________________________\n"
+	a=0
+	for v=1,#result do
+		a=a+1
+		result_text = result_text..a.."- "..result[a].."\n\n"
+	end
+	return result_text.."______________________________\n"
+end
+
+return {
+	description = "Fantasy Writer",
+	usage = {"write [text] : زیبا نویسی",},
+	patterns = {
+		"^[/!#$]([Tt][Ee][Xx][Tt]) (.*)",
+		},
+	run = run
+}
