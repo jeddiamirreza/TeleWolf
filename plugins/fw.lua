317e3c7a07d6410fa6ac5c8d9a3356fba74c4a24
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-06-07 18:11:49 +0430

    Update fw.lua

--- plugins/fw.lua
+++ plugins/fw.lua
@@ -3,6 +3,15 @@ do
 local function pre_process(msg)
     
 
+ local data = load_data(_config.moderation.data)
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['lock_fwd'] then
+                lock_fwd = data[tostring(msg.to.id)]['settings']['lock_fwd']
+            end
+        end
+end
+
     local hash = 'mate:'..msg.to.id
     if redis:get(hash) and msg.fwd_from and not is_sudo(msg) and not is_owner(msg) and not is_momod(msg) and not is_admin1(msg)  then
             delete_msg(msg.id, ok_cb, true)
